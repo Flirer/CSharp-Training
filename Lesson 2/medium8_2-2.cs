faef51f447a6355fc74f68f3bcac9d304a5c5509
flirer <omfgcopter@gmail.com>
2019-10-26 15:20:06 +0300

    Crated Bank and Account classes

--- Lesson 2/medium8_2-2.cs
+++ Lesson 2/medium8_2-2.cs
@@ -1,29 +1,94 @@
-class Wombat
-{
-    public int Health;
-    public int Armor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
-    public void TakeDamage(int damage)
+
+class Bank
+{
+    private List<Account> _accounts;
+    private Stack<List<Account>> _states;
+
+    public Bank()
     {
-        Health -= damage - armor;
-        if (Health <= 0)
-        {
-            Console.WriteLine("Я умер");
-        }
+        _accounts = new List<Account>();
+        _states = new Stack<List<Account>>();
+    }
+
+    public void AddAccount(string name, float amount)
+    {
+        if (_accounts.Exists((account) => account.Name == name.ToUpperInvariant()))
+            throw new Exception("Alredy containt account with name " + name);
+
+        SaveState();
+        _accounts.Add(new Account(name.ToUpperInvariant(), amount));
+    }
+
+    public void MoveCredits(string nameFrom, string nameTo, float amount)
+    {
+        Account from = GetAccount(nameFrom);
+        Account to = GetAccount(nameTo);
+
+        SaveState();
+
+        from.WithdrawCredits(amount);
+        to.DepositCredits(amount);
+    }
+
+    public void CloseAccount(string name)
+    {
+        Account account = GetAccount(name);
+
+        SaveState();
+
+        account.CloseAccount();
+    }
+
+    private Account GetAccount(string name)
+    {
+        Account result = _accounts.FirstOrDefault((account) => account.Name == name);
+
+        if (result == null)
+            throw new Exception("No account with name " + name + " is presented.");
+
+        return result;
+    }
+
+    private void SaveState()
+    {
+        _states.Push(new List<Account>(_accounts));
+    }
+
+    public void Undo()
+    {
+        _accounts = _states.Pop();
     }
 }
 
-class Human
+class Account
 {
-    public int Health;
-    public int Agility;
+    public string Name { get; private set; }
+    public float Amount { get; private set; }
+    public bool IsActive { get; private set; }
 
-    public void TakeDamage(int damage)
+    public Account(string name, float amount)
     {
-        Health -= damage / Agility;
-        if (Health <= 0)
-        {
-            Console.WriteLine("Я умер");
-        }
+        Name = name;
+        Amount = amount;
+        IsActive = true;
+    }
+
+    public void DepositCredits(float amount)
+    {
+        Amount += amount;
+    }
+
+    public void WithdrawCredits(float amount)
+    {
+        Amount -= amount;
+    }
+
+    public void CloseAccount()
+    {
+        IsActive = false;
     }
 }
